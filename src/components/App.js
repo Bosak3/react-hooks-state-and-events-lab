99855104cd54d7890d95b08211493973127dd40f
Adam <bosak3@gmail.com>
2021-12-01 18:47:33 -0600

    commit tree 0720a1e4bcb88bfeda1831da5dd492a5cd7459d4

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,28 @@
-import React from "react";
+import {useState} from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
 
+  const [Mode, setMode] = useState(false)
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = Mode ? "App dark" : "App light"
+  
+
+
+  function handleClick() {
+      setMode(!Mode)
+  }
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleClick}>
+        {Mode ? "Dark" : "Light"} Mode
+        </button>
+          
       </header>
       <ShoppingList items={itemData} />
     </div>
@@ -20,3 +30,26 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+/*Deliverables
+
+  1) Using the useState hook, create a state variable in the App component
+  2) Use that variable to determine if our app should be in dark mode or light mode
+  3) Create an event handler to the dark mode button, and update state when the button
+     clicked
+ 
+  4)In the Item component, when a user clicks the <button>, the item should be added
+    to the cart.
+    **The way to show the user that the item is in the cart, is by changing the 
+    className on the <li> element.
+
+  5)
+
+
+
+*/
